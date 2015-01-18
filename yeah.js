251a0fbd2430a23582b1e31cdf78d86ae814dfe4
jhendley25 <jake.hendley25@gmail.com>
2015-01-18 13:28:19 -0500

    first commit of command line tool

---
+++ yeah.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+var program = require("commander"),
+    iosHelper = require('./lib/iosHelper');
+
+program
+  .version('1.0.0')
+  .usage('emulate')
+  .parse(process.argv);
+
+if(!program.args.length) {
+  program.help();
+} else {
+  init(program.args[0])  
+}
+
+function init(baseCommand){
+  switch(baseCommand) {
+    case "emulate":
+      iosHelper.emulate(program.args[1])
+      break;
+    default:
+      program.help()
+      break;
+  }
+}
+
